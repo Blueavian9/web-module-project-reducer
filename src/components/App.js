ef4615055f390cd1d4af19a9580684bd0c3e8f87
cesar.aguilar <Blueavian9@gmail.com>
2022-08-15 13:10:01 -0700

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import App.js from './reducer/index.js';
-import App.js from './action/index.js';
+
 
 function App() {
   return (
