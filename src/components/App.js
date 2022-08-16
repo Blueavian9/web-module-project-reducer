a810e26ba839295695ee2c41bd469d10533f088b
cesar.aguilar <Blueavian9@gmail.com>
2022-08-15 17:48:37 -0700

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 import reducer, { initialState } from './reducers';
-import { applyNumber, changeOperation } from './actions';
+import { applyNumber, changeOperation, clearDisplay } from './actions';
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState); 
@@ -16,6 +16,10 @@ function App() {
   }
   const handleOperationClick = (operator) => {
   dispatch(changeOperation(operator)); 
+  }
+
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
   }
 
   return (
@@ -65,7 +69,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={()=>handleClearDisplay("CE")}/>
             </div>
 
           </form>
